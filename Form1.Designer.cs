a6ed89bd653bdca85689c077661a2a76ed35c058
Seid723 <seidseid976@gmail.com>
2025-08-12 23:10:33 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace InventoryMgmt
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            button1 = new Button();
+            maskedTextBox1 = new MaskedTextBox();
+            label3 = new Label();
+            label2 = new Label();
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            checkBox1 = new CheckBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(255, 128, 0);
+            panel1.Controls.Add(checkBox1);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(maskedTextBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(richTextBox1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(127, 67);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(422, 479);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DodgerBlue;
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.Cursor = Cursors.Hand;
+            button1.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(57, 352);
+            button1.Name = "button1";
+            button1.Size = new Size(312, 47);
+            button1.TabIndex = 6;
+            button1.Text = "LOGIN";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.BackColor = SystemColors.Highlight;
+            maskedTextBox1.BorderStyle = BorderStyle.FixedSingle;
+            maskedTextBox1.Cursor = Cursors.IBeam;
+            maskedTextBox1.Location = new Point(57, 243);
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.PasswordChar = '●';
+            maskedTextBox1.Size = new Size(312, 27);
+            maskedTextBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(57, 209);
+            label3.Name = "label3";
+            label3.Size = new Size(112, 31);
+            label3.TabIndex = 4;
+            label3.Text = "Password";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(57, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 31);
+            label2.TabIndex = 2;
+            label2.Text = "Username";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = SystemColors.Highlight;
+            richTextBox1.BorderStyle = BorderStyle.None;
+            richTextBox1.Cursor = Cursors.IBeam;
+            richTextBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            richTextBox1.ForeColor = SystemColors.InfoText;
+            richTextBox1.Location = new Point(57, 149);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ScrollBars = RichTextBoxScrollBars.None;
+            richTextBox1.Size = new Size(312, 27);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(160, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Login";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Cursor = Cursors.Hand;
+            checkBox1.Location = new Point(237, 276);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(132, 24);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "Show Password";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(712, 663);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private RichTextBox richTextBox1;
+        private Label label2;
+        private MaskedTextBox maskedTextBox1;
+        private Label label3;
+        private Button button1;
+        private CheckBox checkBox1;
+    }
+}
