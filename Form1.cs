88d4fd456d04caedd4e7f74831c88e37f72c69b6
Seid723 <seidseid976@gmail.com>
2025-08-12 23:10:21 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+namespace InventoryMgmt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == false)
+                PasswordTb
+        }
+    }
+}
